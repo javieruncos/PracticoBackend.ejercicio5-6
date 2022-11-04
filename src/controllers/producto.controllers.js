cf1908959c5cb72aa79fd82a34a92fd00baed387
Javier Uncos <javiersrfc30@gmail.com>
2022-11-04 11:56:11 -0300

    agregamos las validaciones al POST y al PUT

--- src/controllers/producto.controllers.js
+++ src/controllers/producto.controllers.js
@@ -1,3 +1,4 @@
+import { validationResult } from "express-validator";
 import Receta from "../models/receta";
 
 
@@ -5,6 +6,18 @@ import Receta from "../models/receta";
 
 export const crearReceta = async(req,res)=>{
      try {
+        
+
+         const errors = validationResult(req)
+         if(!errors.isEmpty()){
+            return res.status(400).json({
+                errors:errors.array()
+            })
+         }
+
+
+
+
         const nuevaReceta = new Receta(req.body)
         await nuevaReceta.save()
         res.status(201).json({
@@ -50,7 +63,12 @@ export const obtenerProducto = async(req,res)=>{
 
 export const editarReceta =async (req,res)=>{
        try {
-
+           const errors = validationResult(req)
+           if(!errors.isEmpty()){
+            return res.status(400).json({
+                errores: errors.array()
+            })
+           }
          await Receta.findByIdAndUpdate(req.params.id,req.body)
           res.status(200).json({
             mensaje:"el producto fue correctamente editado"
--- src/routes/recetas.routes.js
+++ src/routes/recetas.routes.js
@@ -1,10 +1,74 @@
 import { Router } from "express";
-import { borrarProducto, crearReceta, editarReceta, listarRecetas, obtenerProducto } from "../controllers/producto.controllers";
-
+import {
+  borrarProducto,
+  crearReceta,
+  editarReceta,
+  listarRecetas,
+  obtenerProducto,
+} from "../controllers/producto.controllers";
+import { check } from "express-validator";
 const router = Router();
 
-router.route("/receta").get(listarRecetas).post(crearReceta)
-router.route("/receta/:id").get(obtenerProducto).put(editarReceta).delete(borrarProducto)
-
+router
+  .route("/receta")
+  .get(listarRecetas)
+  .post(
+    [
+      check("nombreReceta")
+        .notEmpty()
+        .withMessage("el nombre de la receta es obligatorio")
+        .isLength({ min: 3, max: 300 })
+        .withMessage(
+          "la cantidad minima de caracteres debe estar entre 3 y 300"
+        ),
+      check("imagen")
+        .notEmpty()
+        .withMessage("La url de la imagen es obligatoria")
+        .matches(/^https?:\/\/[\w\-]+(\.[\w\-]+)+[/#?]?.*$/)
+        .withMessage("debe ingresar una url valida"),
+      check("ingredientes")
+        .notEmpty()
+        .withMessage("los ingredientes son obligatorios")
+        .isLength({ min: 10, max: 600 })
+        .withMessage("La cantidad de caracteres debe estar entre 10 y 600"),
+      check("categoria")
+        .notEmpty()
+        .withMessage("la categoria es obligatoria")
+        .isIn("Ensaladas", "Pollo", "Pastas", "Guisos", "Postres")
+        .withMessage("debe ingresar una categoria valida"),
+    ],
+    crearReceta
+  );
+router
+  .route("/receta/:id")
+  .get(obtenerProducto)
+  .put(
+    [
+      check("nombreReceta")
+        .notEmpty()
+        .withMessage("el nombre de la receta es obligatorio")
+        .isLength({ min: 3, max: 300 })
+        .withMessage(
+          "la cantidad minima de caracteres debe estar entre 10 y 300"
+        ),
+      check("imagen")
+        .notEmpty()
+        .withMessage("La url de la imagen es obligatoria")
+        .matches(/^https?:\/\/[\w\-]+(\.[\w\-]+)+[/#?]?.*$/)
+        .withMessage("debe ingresar una url valida"),
+      check("ingredientes")
+        .notEmpty()
+        .withMessage("los ingredientes son obligatorios")
+        .isLength({ min: 10, max: 600 })
+        .withMessage("La cantidad de caracteres debe estar entre 10 y 600"),
+      check("categoria")
+        .notEmpty()
+        .withMessage("la categoria es obligatoria")
+        .isIn("Ensaladas", "Pollo", "Pastas", "Guisos", "Postres")
+        .withMessage("debe ingresar una categoria valida"),
+    ],
+    editarReceta
+  )
+  .delete(borrarProducto);
 
 export default router;
